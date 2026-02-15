82cd339436b7cf849ec8563d9f4aa125b131ddf0
Shraddha Sasikumar <shraddhaskumar04@gmail.com>
2026-02-15 18:34:49 +0530

    Updated message

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask via Kubernetes + ArgoCD!"
+    return "Hello from Flask via Kubernetes + ArgoCD! Updated!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
