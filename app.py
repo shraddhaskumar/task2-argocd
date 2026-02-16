839ce8960af0590a322bff84e36653dc092112c3
Shraddha Sasikumar <shraddhaskumar04@gmail.com>
2026-02-16 08:34:58 +0530

    Updated Flask app message to version 2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Flask + ArgoCD! Updated version!"
+    return "Hello from Flask + ArgoCD! Version 2!"
 
 
 if __name__ == '__main__':
